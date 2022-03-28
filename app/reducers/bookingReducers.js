31bada764fefc0f764f0bd87163b2647a2871d0d
ngahika01 <mykenjoroge01@gmail.com>
2022-03-28 16:06:09 +0300

    Addded booking reducer

--- app/reducers/bookingReducers.js
+++ app/reducers/bookingReducers.js
@@ -3,6 +3,21 @@ import {
   BOOKING_CREATE_REQUEST,
   BOOKING_CREATE_RESET,
   BOOKING_CREATE_SUCCESS,
+  BOOKING_DELETE_FAILURE,
+  BOOKING_DELETE_REQUEST,
+  BOOKING_DELETE_RESET,
+  BOOKING_DELETE_SUCCESS,
+  BOOKING_GET_FAIL,
+  BOOKING_GET_REQUEST,
+  BOOKING_GET_RESET,
+  BOOKING_GET_SUCCESS,
+  BOOKING_LIST_FAIL,
+  BOOKING_LIST_REQUEST,
+  BOOKING_LIST_SUCCESS,
+  BOOKING_UPDATE_FAILURE,
+  BOOKING_UPDATE_REQUEST,
+  BOOKING_UPDATE_RESET,
+  BOOKING_UPDATE_SUCCESS,
 } from "../constants/bookingConstants";
 
 export const bookingCreateReducer = (state = {}, action) => {
@@ -32,3 +47,113 @@ export const bookingCreateReducer = (state = {}, action) => {
       return {};
   }
 };
+
+export const bookingUpdateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case BOOKING_UPDATE_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case BOOKING_UPDATE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        success: true,
+        booking: action.payload,
+      };
+    default:
+      return state;
+
+    case BOOKING_UPDATE_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case BOOKING_UPDATE_RESET:
+      return {};
+  }
+};
+
+export const bookingDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case BOOKING_DELETE_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case BOOKING_DELETE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        success: true,
+        booking: action.payload,
+      };
+    default:
+      return state;
+
+    case BOOKING_DELETE_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case BOOKING_DELETE_RESET:
+      return {};
+  }
+}
+
+export const bookingGetReducer = (state = {}, action) => {
+  switch (action.type) {
+    case BOOKING_GET_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case BOOKING_GET_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        success: true,
+        booking: action.payload,
+      };
+    default:
+      return state;
+
+    case BOOKING_GET_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case BOOKING_GET_RESET:
+      return {};
+  }
+}
+
+export const bookingListReducer = (state = {}, action) => {
+  switch (action.type) {
+    case BOOKING_LIST_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case BOOKING_LIST_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        success: true,
+        bookings: action.payload,
+      };
+    default:
+      return state;
+
+    case BOOKING_LIST_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+  }
+}
